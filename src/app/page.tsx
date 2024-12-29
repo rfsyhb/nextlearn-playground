0c463b2d86f910f205d7d7fa8a02b83acaa6e7e8
Rafi Syihab <rafisyihab3@gmail.com>
2024-12-29 12:42:10 +0700

    up: metadata, root page, color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,16 @@ import Image from 'next/image';
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <div className="font-mono font-semibold">
-      <Image src="https://i.imgur.com/4YLbTtR.jpeg" alt="banner" width={400} height={400} />
-      <p>WAASUUUP</p>
+      <header className='flex flex-col items-center justify-center gap-2'>
+        <Image
+          src="https://i.imgur.com/4YLbTtR.jpeg"
+          alt="banner"
+          width={900}
+          height={100}
+          className='rounded-lg object-cover max-h-[22vh] w-full'
+        />
+        <p className='text-xl absolute bg-foreground text-background p-4 py-2 rounded-md opacity-80 hover:opacity-100'>&#64;rfsyhb&apos;s playground</p>
+      </header>
       {children}
     </div>
   );
